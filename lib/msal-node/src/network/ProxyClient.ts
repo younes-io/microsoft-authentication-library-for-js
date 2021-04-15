a63c1f5a182dcd1a7aba7ca6c754d5dc48361c92
sameerag <sameera.gajjarapu@microsoft.com>
2021-04-15 11:56:45 -0700

    First draft

---
+++ lib/msal-node/src/network/ProxyClient.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+    INetworkModule,
+    NetworkRequestOptions,
+    NetworkResponse,
+} from "@azure/msal-common";
+import { HttpMethod } from "../utils/Constants";
+import axios, { AxiosRequestConfig } from "axios";
+
+/**
+ * This class implements the API for network requests.
+ */
+export class ProxyClient implements INetworkModule {
+
+    /**
+     * Http Get request
+     * @param url
+     * @param options
+     */
+    async sendGetRequestAsync<T>(
+        url: string,
+        options?: NetworkRequestOptions
+    ): Promise<NetworkResponse<T>> {
+        const request: AxiosRequestConfig = {
+            method: HttpMethod.GET,
+            url: url,
+            headers: options && options.headers,
+            proxy: {
+                host: 'localhost',
+                port: 4000
+            },
+            validateStatus: () => true
+        };
+
+        const response = await axios(request);
+        return {
+            headers: response.headers,
+            body: response.data as T,
+            status: response.status,
+        };
+    }
+
+    /**
+     * Http Post request
+     * @param url
+     * @param options
+     */
+    async sendPostRequestAsync<T>(
+        url: string,
+        options?: NetworkRequestOptions
+    ): Promise<NetworkResponse<T>> {
+        const request: AxiosRequestConfig = {
+            method: HttpMethod.POST,
+            url: url,
+            data: (options && options.body) || "",
+            headers: options && options.headers,
+            proxy: {
+                host: 'localhost',
+                port: 4000
+            },
+            validateStatus: () => true
+        };
+
+        const response = await axios(request);
+        return {
+            headers: response.headers,
+            body: response.data as T,
+            status: response.status,
+        };
+    }
+}
--- lib/msal-node/src/utils/NetworkUtils.ts
+++ lib/msal-node/src/utils/NetworkUtils.ts
@@ -4,13 +4,15 @@
  */
 
 import { INetworkModule } from "@azure/msal-common";
-import { HttpClient } from "../network/HttpClient";
+// import { HttpClient } from "../network/HttpClient";
+import { ProxyClient } from "../network/ProxyClient";
 
 export class NetworkUtils {
     /**
      * Returns best compatible network client object.
      */
     static getNetworkClient(): INetworkModule {
-        return new HttpClient();
+        // return new HttpClient();
+        return new ProxyClient();
     }
 }
