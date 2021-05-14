886380da4e6f96978f88668af5bb2a1c9cf6e981
HectorMg <hemoral@microsoft.com>
2021-05-13 18:03:09 -0700

    Add bound token response decryption step to support Bound RT protocol

--- lib/msal-browser/src/crypto/JsonWebEncryption.ts
+++ lib/msal-browser/src/crypto/JsonWebEncryption.ts
@@ -101,27 +101,23 @@ export class JsonWebEncryption {
     }
 
     async decrypt(decryptionKey: CryptoKey): Promise<string> {
-        const cipherText = BrowserStringUtils.stringToArrayBuffer(this.ciphertext);
-        const iv = BrowserStringUtils.stringToArrayBuffer(this.initializationVector);
+        const cipherText = new Uint8Array(BrowserStringUtils.stringToArrayBuffer(this.ciphertext));
+        const authenticationTag = new Uint8Array(BrowserStringUtils.stringToArrayBuffer(this.authenticationTag));
+        const encryptedData = new Uint8Array(cipherText.byteLength + authenticationTag.byteLength);
+        encryptedData.set(cipherText, 0);
+        encryptedData.set(authenticationTag, cipherText.byteLength);
+        const iv = new Uint8Array(BrowserStringUtils.stringToArrayBuffer(this.initializationVector));
         const additionalData = this.authenticatedData;
-        console.log(this.ciphertext);
-        console.log(this.authenticationTag);
-        console.log("AD:", additionalData);
-        const authenticationTag = BrowserStringUtils.stringToArrayBuffer(this.authenticationTag);
 
         const aesGcmParams: AesGcmParams = {
             name: "AES-GCM",
-            iv:  iv,
+            iv: iv,
             additionalData: additionalData,
             tagLength: authenticationTag.byteLength * 8
         };
-        console.log("CT:", cipherText);
-        console.log("AESGCMParams: ", aesGcmParams);
-        const responseBuffer = window.crypto.subtle.decrypt(aesGcmParams, decryptionKey, cipherText);
-        console.log("Response buffer: ", responseBuffer);
-        const responseString = BrowserStringUtils.utf8ArrToString(new Uint8Array(await responseBuffer));
-        console.log(responseString);
-        return JSON.parse(responseString);
+
+        const responseBuffer = await window.crypto.subtle.decrypt(aesGcmParams, decryptionKey, encryptedData);
+        return BrowserStringUtils.utf8ArrToString(new Uint8Array(responseBuffer));
     }
 
     private parseJweProtectedHeader(encodedHeader: string): JoseHeader {
